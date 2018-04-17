86bafd4305d839381ea79a1845ee691c1cf26dc7
Ivan BOHONOSIUK <ibohonos@e2r2p2.unit.ua>
2018-04-17 13:47:21 +0300

    some bugs

--- srcs/ft_find_road.c
+++ srcs/ft_find_road.c
@@ -6,7 +6,7 @@
 /*   By: ibohonos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 18:29:53 by ibohonos          #+#    #+#             */
-/*   Updated: 2018/04/15 20:10:12 by ibohonos         ###   ########.fr       */
+/*   Updated: 2018/04/17 12:56:24 by ibohonos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_full_road(t_rooms *end, t_rooms *r, t_rooms *l)
 	// r = p->start_room;			// починаючи з вузла-джерела
 	r->free = 0;
 	queue = r;
-	l = queue;
+	l = r;
 	while (queue)		// поки черга не порожня
 	{
 		if (queue == end)
@@ -28,9 +28,11 @@ int		ft_full_road(t_rooms *end, t_rooms *r, t_rooms *l)
 		{
 			if (queue->link->room->free)	// ... які ще не були відвідані ...
 			{
+				l = l->next;
 				ft_printf("room = %s\n", queue->link->room->name);
 				queue->link->room->free = 0;	// ... позначити як відвідані
 				queue = queue->link->room;		// ... додати в кінець черги...
+				l = queue;
 				// queue.push(child);
 			}
 			else
@@ -49,6 +51,13 @@ void	ft_find_road(t_in *p, t_rooms *r)
 	ft_printf("\n");
 	i = ft_full_road(p->end_room, p->start_room, &l);
 	r = NULL;
+	i = 0;
+	while (i < 4)
+	{
+		ft_printf("name = %s\n", l.name);
+		l = *((&l)->next);
+		i++;
+	}
 	// while (r)
 	// {
 	// 	while (r->link)
