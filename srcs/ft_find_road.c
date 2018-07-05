01e21d6c67b8a805f777a80e2d21cad7cd200422
ibohonos <ibohonos@e2r11p12.unit.ua>
2018-07-05 19:11:36 +0300

    broke find road

--- srcs/ft_find_road.c
+++ srcs/ft_find_road.c
@@ -12,74 +12,78 @@
 
 #include "lem_in.h"
 
-// int		ft_full_road(t_rooms *end, t_rooms *r, t_rooms *l)
-// {
-// 	t_rooms *queue;
-
-// 	// r = p->start_room;			// починаючи з вузла-джерела
-// 	r->free = 0;
-// 	queue = r;
-// 	l = r;
-// 	while (queue)		// поки черга не порожня
-// 	{
-// 		if (queue == end)
-// 			return (1);			// перевірити, чи не є поточний вузол цільовим
-// 		while (queue->link)		// всі наступники поточного вузла, ...
-// 		{
-// 			if (queue->link->room->free)	// ... які ще не були відвідані ...
-// 			{
-// 				l = l->next;
-// 				ft_printf("room = %s\n", queue->link->room->name);
-// 				queue->link->room->free = 0;	// ... позначити як відвідані
-// 				queue = queue->link->room;		// ... додати в кінець черги...
-// 				l = queue;
-// 				// queue.push(child);
-// 			}
-// 			else
-// 				break ;
-// 		}
-// 		queue = queue->next;
-// 	}
-// 	return (0);					// цільовий вузол недосяжний
-// }
-
-void	ft_find_road(t_in *p, t_rooms *r)
+static int	ft_full_road(t_rooms *end, t_rooms *r, t_rooms *l)
 {
-	// int		i;
-	// t_rooms	l;
+	t_rooms *queue;
+
+	// r = p->start_room;			// починаючи з вузла-джерела
+	r->free = 0;
+	queue = r;
+	l = r;
+	while (queue)		// поки черга не порожня
+	{
+		if (queue == end)
+			return (1);			// перевірити, чи не є поточний вузол цільовим
+		while (queue->link)		// всі наступники поточного вузла, ...
+		{
+			if (queue->link->room->free)	// ... які ще не були відвідані ...
+			{
+				l = l->next;
+				ft_printf("room = %s\n", queue->link->room->name);
+				queue->link->room->free = 0;	// ... позначити як відвідані
+				queue = queue->link->room;		// ... додати в кінець черги...
+				l = queue;
+				// queue.push(child);
+			}
+			else
+			{
+				ft_printf("breack room = %s\n", queue->link->room->name);
+				break ;
+			}
+		}
+		queue = queue->next;
+	}
+	return (0);					// цільовий вузол недосяжний
+}
+
+void		ft_find_road(t_in *p, t_rooms *r)
+{
+	int		i;
+	t_rooms	l;
 
 	ft_printf("\n");
-	// i = ft_full_road(p->end_room, p->start_room, &l);
-	p = NULL;
+	i = ft_full_road(p->end_room, p->start_room, &l);
+	// p = NULL;
 	// r = NULL;
 	// i = 0;
-	while (r != NULL)
-	{
-		ft_printf("name = %s\n", r->name);
-		while (r->link != NULL)
-		{
-			ft_printf("link name = %s\n", r->link->room->name);
-			r->link = r->link->next;
-		}
-		r = r->next;
-		// i++;
-	}
-	// while (r)
+	// while (r != NULL)
 	// {
-	// 	while (r->link)
+	// 	ft_printf("name = %s\n", r->name);
+	// 	while (r->link != NULL)
 	// 	{
-	// 		if (!r->link->room->free && r->link->room != p->end_room)
-	// 		{
-	// 			ft_printf("name = %s\n", r->link->room->name);
-	// 			r = r->link->room;
-	// 		}
-	// 		else
-	// 			break ;
-	// 		// ft_printf("name 3 = %s\n", r->link->room->name);
-	// 		// r->link = r->link->next;
+	// 		ft_printf("link name = %s\n", r->link->room->name);
+	// 		r->link = r->link->next;
 	// 	}
-	// 	// ft_printf("name4 = %s\n", r->name);
 	// 	r = r->next;
+	// 	// i++;
 	// }
-	// ft_printf("i = %d\n", i);
+	ft_printf("\n");
+	while (l)
+	{
+		while (l.link)
+		{
+			if (!l.link->room->free && l.link->room != p->end_room)
+			{
+				ft_printf("name = %s\n", l.link->room->name);
+				l = &(l)->link->room;
+			}
+			else
+				break ;
+			// ft_printf("name 3 = %s\n", r->link->room->name);
+			l->link = l->link->next;
+		}
+		// ft_printf("name4 = %s\n", r->name);
+		l = l->next;
+	}
+	ft_printf("i = %d\n", i);
 }
